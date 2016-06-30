16ad899361bb7212ff0fcc2d2af21b4312d8e2a5
Kurt Bautista <kurtv.bautista@gmail.com>
2016-06-30 16:17:36 +0800

    new classes

---
+++ app/src/main/java/com/kurtbautista/lab4/Food.java
@@ -0,0 +1,53 @@
+package com.kurtbautista.lab4;
+
+/**
+ * Created by Student on 6/30/2016.
+ */
+public class Food {
+
+    private String name;
+    private double price;
+    private String description;
+    private int rating;
+
+    public Food(String name, double price, String description, int rating)
+    {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+}
--- app/src/main/java/com/kurtbautista/lab4/MainActivity.java
+++ app/src/main/java/com/kurtbautista/lab4/MainActivity.java
@@ -1,9 +1,15 @@
 package com.kurtbautista.lab4;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Activity context;
+    private ArrayList<Food> foodList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
